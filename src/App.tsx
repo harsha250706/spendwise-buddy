bfe78ab4b50e88c0ea2632281507ff6a10a5bbce
harsha250706 <harshaclg07@gmail.com>
2026-08-25 20:32:35 +0530

    Import DesktopSidebar component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DesktopSidebar from "./components/DesktopSidebar";
 
 import Index from "./pages/Index";
 import InsightsPage from "./pages/InsightsPage";
